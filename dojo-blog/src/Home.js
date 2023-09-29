b8f5459407e68404962f400d03553a8795f05787
Flavio Santana <flavio.santana@pixforce.ai>
2023-09-29 02:27:14 -0300

    Add routing to App

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import useFetch from "./lessons/useFetch"
+import React from "react";
+import useFetch from "./lessons/useFetch";
 import BlogList from "./lessons/BlogList";
 
 const Home = () => {
