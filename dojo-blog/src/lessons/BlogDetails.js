e5b8fdb6f4fc21e9c7f64c89933c24da6a3a3080
Flavio Santana <flavio.santana@pixforce.ai>
2023-09-30 00:48:19 -0300

    Fetch data and display in BlogDetails page

--- dojo-blog/src/lessons/BlogDetails.js
+++ dojo-blog/src/lessons/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data: blog, isPending, errorMessage } = useFetch('http://localhost:8000/blogs/' + id);
     return (
         <div className="blog-details">
-            <h2>Blog Details: id {id}</h2>
+            <div>{isPending && <p>Loading...</p>}</div>
+            <div>{errorMessage}</div>
+            {blog &&
+                <div>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                    <div>{blog.body}</div>
+                </div>
+            }
         </div>
     );
 }
