e1b4a993edde71028deb0be70b481cc1f0a5e26f
Flavio Santana <flavio.santana@pixforce.ai>
2023-10-02 01:18:26 -0300

    Implement blog post

--- dojo-blog/src/lessons/Create.js
+++ dojo-blog/src/lessons/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = {title, body, author};
-        console.log(blog);
+        const blog = { title, body, author };
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(blog)
+        }).finally(() => {
+            setIsPending(false);
+        })
     }
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
@@ -35,7 +45,8 @@ const Create = () => {
                     <option value='mario'>mario</option>
                     <option value='yoshi'>yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button>...</button>}
             </form>
             <p>{title}</p>
             <p>{body}</p>
